a75bb7853d0985154b21df6724c26c650ed79c49
Michael Groufsky <m.groufsky@gmail.com>
2026-08-15 14:53:22 +1200

    VERDIGRIS: Upgrade to 8abfb8d69c689e2c607225c98baa829f839686f9

--- external/verdigris/wobjectdefs.h
+++ external/verdigris/wobjectdefs.h
@@ -290,16 +290,28 @@ constexpr std::integral_constant<int, int(w_internal::PropertyFlags::Final)> W_F
 
 namespace w_internal {
 
+#if QT_VERSION >= QT_VERSION_CHECK(6,2,0)
+template <class Func> struct isConstMemberMethod : std::false_type {};
+template <class Ret, class Obj, class...Args> struct isConstMemberMethod<Ret (Obj::*)(Args...) const> : std::true_type {};
+template <class Ret, class Obj, class...Args> struct isConstMemberMethod<Ret (Obj::*)(Args...) const noexcept> : std::true_type {};
+#endif
+
 /// Holds information about a method
 template<typename F, int Flags, typename IC, typename ParamTypes, typename ParamNames = StringViewArray<>>
 struct MetaMethodInfo {
-    F func;
+    using Func = F;
+    Func func;
     StringView name;
     ParamTypes paramTypes;
     ParamNames paramNames;
     static constexpr int argCount = QtPrivate::FunctionPointer<F>::ArgumentCount;
-    static constexpr auto argSequence = make_index_sequence<argCount>{};
+    using ArgSequence = make_index_sequence<argCount>;
+    static constexpr auto argSequence = ArgSequence{};
+#if QT_VERSION >= QT_VERSION_CHECK(6,2,0)
+    static constexpr int flags = Flags + (isConstMemberMethod<F>::value ? 0x100 : 0);
+#else
     static constexpr int flags = Flags;
+#endif
     using IntegralConstant = IC;
 };
 
@@ -338,7 +350,7 @@ constexpr MetaConstructorInfo<Args...> makeMetaConstructorInfo(StringView name)
 { return { name }; }
 
 struct Empty{
-    constexpr operator bool() const { return false; }
+    constexpr bool operator!=(std::nullptr_t) const { return false; }
 };
 
 /// Holds information about a property
@@ -584,6 +596,24 @@ template <typename... Args> constexpr QOverload<Args...> qOverload = {};
 } // namespace w_internal
 
 #if defined(Q_CC_MSVC) && !defined(Q_CC_CLANG)
+  #if _MSC_VER >= 1928
+    #if defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL
+      // MSVC in /Zc:preprocessor- mode (default)
+      #define W_CONFORMING_PREPROCESSOR 0
+    #else
+      // MSVC in /Zc:preprocessor mode
+      #define W_CONFORMING_PREPROCESSOR 1
+    #endif
+  #else
+    // Older versions of MSVC without the /Zc:preprocessor flag
+    #define W_CONFORMING_PREPROCESSOR 0
+  #endif
+#else
+  // Other compilers, generally conforming
+  #define W_CONFORMING_PREPROCESSOR 1
+#endif
+
+#if !W_CONFORMING_PREPROCESSOR
 // Workaround for MSVC: expension rules are different so we need some extra macro.
 #define W_MACRO_MSVC_EXPAND(...) __VA_ARGS__
 #define W_MACRO_MSVC_DELAY(X,...) W_MACRO_MSVC_EXPAND(X(__VA_ARGS__))
